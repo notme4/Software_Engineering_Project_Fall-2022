f08134f7ab418980b1dd24497d1d64efd8526ca3
notme4 <102337330+notme4@users.noreply.github.com>
2022-11-11 18:35:39 -0600

    fixed timmy's atrocious tab situation

--- src/SearchingController.js
+++ src/SearchingController.js
@@ -7,70 +7,71 @@ let originalTopic;
 let topicArr = [];
 let deleteFiller = ['what', 'is', 'are', 'the', 'who', 'when', 'where', 'how'];
 class SearchingTopic{
-    constructor() {
-         originalTopic = "";
-         inputTopic = "";
-    }
-    static search(topic) {
-  	     originalTopic = topic;
-         let tempInputTopic;
-         inputTopic = topic.toLowerCase();
-         topicArr = inputTopic.split(" ");
-         keywords(topicArr);
-         inputTopic = topicArr.split(",");
-   
-         topicArr = inputTopic.split(" ");
-         searchMatch(topicArr);
-    }
-    static keywords(topicArr) {
-  	     let index;
-  	     for(let i = 0; i < deleteFiller.length; i++) {
-    	        index = topicArr.indexOf(deleteFiller[i]);
-              topicArr.splice(index, 1);
-         }
-    }
-    static searchMatch(searchArr) {
-  	     let contains = "false";
-         let condition = "true";
-         if (inputTopic == "") {
-    	        console.log("Your search of " + originalTopic + " generated no results.\n");
-              return;
-         }
-    
-         console.log("Your search of: " + origintalTopic + " generated -- ");
-         for (let i = 0; i < searchArr.length && condition == "true"; i++) {
-    	        if (topic1.includes(searchArr[i])) {
-      	           contains = "true";
-                   condition = "false";
-              }
-         }
-         if (contains == "true") {
-    	        console.log(topic1 + "\n");
-              contains = "false";
-              condition = "true";
-         }
-         for (let i = 0; i < searchArr.length && condition == "true"; i++) {
-    	        if (topic2.includes(searchArr[i])) {
-      	           contains = "true";
-                   condition = "false";
-              }
-         }
-         if (contains == "true") {
-    	        console.log(topic2 + "\n");
-              contains = "false";
-              condition = "true";
-         }
-         for (let i = 0; i < searchArr.length && condition == "true"; i++) {
-    	        if (topic3.includes(searchArr[i])) {
-      	           contains = "true";
-                   condition = "false";
-              }
-         }
-         if (contains == "true") {
-    	        console.log(topic3 + "\n");
-              contains = "false";
-              condition = "true";
-         }
-     }
+	constructor() {
+		originalTopic = "";
+		inputTopic = "";
+	}
+	static search(topic) {
+		originalTopic = topic;
+		let tempInputTopic;
+		inputTopic = topic.toLowerCase();
+		topicArr = inputTopic.split(" ");
+		keywords(topicArr);
+		inputTopic = topicArr.split(",");
+
+		topicArr = inputTopic.split(" ");
+		searchMatch(topicArr);
+	}
+	static keywords(topicArr) {
+		let index;
+		for(let i = 0; i < deleteFiller.length; i++) {
+			index = topicArr.indexOf(deleteFiller[i]);
+			topicArr.splice(index, 1);
+		}
+	}
+	static searchMatch(searchArr) {
+		let contains = "false";
+		let condition = "true";
+		if (inputTopic == "") {
+			console.log("Your search of " + originalTopic + " generated no results.\n");
+			return;
+		}
+	
+		console.log("Your search of: " + origintalTopic + " generated -- ");
+		for (let i = 0; i < searchArr.length && condition == "true"; i++) {
+			if (topic1.includes(searchArr[i])) {
+				contains = "true";
+				condition = "false";
+			}
+		}
+		if (contains == "true") {
+			console.log(topic1 + "\n");
+			contains = "false";
+			condition = "true";
+		}
+		for (let i = 0; i < searchArr.length && condition == "true"; i++) {
+			if (topic2.includes(searchArr[i])) {
+				contains = "true";
+				condition = "false";
+			}
+		}
+		if (contains == "true") {
+			console.log(topic2 + "\n");
+			contains = "false";
+			condition = "true";
+		}
+		for (let i = 0; i < searchArr.length && condition == "true"; i++) {
+			if (topic3.includes(searchArr[i])) {
+				contains = "true";
+				condition = "false";
+			}
+		}
+		if (contains == "true") {
+			console.log(topic3 + "\n");
+			contains = "false";
+			condition = "true";
+		}
+	}
 }
+
 console.log(topic1);
