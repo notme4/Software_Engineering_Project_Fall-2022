36196190305d86a54780207db6b35d584b55b498
notme4 <102337330+notme4@users.noreply.github.com>
2022-12-02 21:12:47 -0600

    delete old ansQ GUI

--- src/SearchingController.js
+++ src/SearchingController.js
@@ -11,81 +11,6 @@ class SearchingTopic{
 		originalTopic = "";
 		inputTopic = "";
 	}
-<<<<<<< HEAD
-	search(topic) {
-		originalTopic = topic;
-		inputTopic = topic.toLowerCase();
-		topicArr = inputTopic.split(" ");
-		this.keywords(topicArr);
-		inputTopic = inputTopic.replace(/,/g, " ");
-		inputTopic = inputTopic.replace(/\?/g, "");
-		topicArr = inputTopic.split(" ");
-		this.searchMatch(topicArr);
-	}
-	keywords(topicArr) {
-  		let index = -1;
-  		for(let i = 0; i < deleteFiller.length; i++) {
-			index = topicArr.indexOf(deleteFiller[i]);
-			if (index != -1) {
-				searchArr.splice(index, 1);
-			}
-			index = -1;
-		}
-		inputTopic = searchArr.toString();
-	}
-	searchMatch(searchArr) {
-		let generations = 0;
-  		let contains = "false";
-		let condition = "true";
-		let i;
-		
-		for(i in searchArr) {
-			searchArr[i] = "\\b" + searchArr[i] + "\\b";
-		}
-		
-		console.log("Your search of: " + originalTopic + " generated -- ");
-		for (let i = 0; i < searchArr.length && condition == true; i++) {
-			if (topic1.search(searchArr[i]) != -1) {
-	  			contains = true;
-				condition = false;
-			}
-		}
-		if (contains == true) {
-			console.log(topic1);
-			contains = false;
-			condition = true;
-			generations++;
-		}
-		for (let i = 0; i < searchArr.length && condition == true; i++) {
-			if (topic2.search(searchArr[i]) != -1) {
-	  			contains = true;
-				condition = false;
-			}
-		}
-		if (contains == true) {
-			console.log(topic2);
-			contains = false;
-			condition = true;
-			generations++;
-		}
-		for (let i = 0; i < searchArr.length && condition == true; i++) {
-			if (topic3.search(searchArr[i]) != -1) {
-	  			contains = true;
-				condition = false;
-			}
-		}
-		if (contains == true) {
-			console.log(topic3);
-			contains = false;
-			condition = true;
-			generations++;
-		}
-		if (generations == 0) {
-			console.log("no results.");
-		}
-		else {
-			console.log("You have " + generations + " result(s).");
-=======
 	static search(topic) {
 		originalTopic = topic;
 		let tempInputTopic;
@@ -145,12 +70,8 @@ class SearchingTopic{
 			console.log(topic3 + "\n");
 			contains = "false";
 			condition = "true";
->>>>>>> AnsweringAQuestionUseCase
 		}
 	}
 }
 
-<<<<<<< HEAD
-=======
 console.log(topic1);
->>>>>>> AnsweringAQuestionUseCase
